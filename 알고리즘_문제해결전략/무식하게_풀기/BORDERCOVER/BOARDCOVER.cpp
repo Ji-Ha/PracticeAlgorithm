c61c466470a3c157f38febf3e6cd363571a73693
Ji-Ha <xodhkd36@gmail.com>
2021-01-10 21:04:39 +0900

    BOARDCOVER : 모든 타일을 다 채우는 경우의 수 구하기

---
+++ 알고리즘_문제해결전략/무식하게_풀기/BORDERCOVER/BOARDCOVER.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+const int coverType[4][3][2] = {
+    {{0, 0}, {1, 0}, {0, 1}},
+    {{0, 0}, {0, 1}, {1, 1}},
+    {{0, 0}, {1, 0}, {1, 1}},
+    {{0, 0}, {1, 0}, {1, -1}}
+};
+
+bool set(vector<vector<int>> &board, int y, int x, int type, int delta){
+  bool ok = true;
+  for (int i = 0; i < 3; i++)
+  {
+    int ny = y + coverType[type][i][0];
+    int nx = x + coverType[type][i][1];
+
+    if(ny < 0 || ny >= board.size() || nx < 0 || nx >= board[0].size())
+      ok = false;
+    else if((board[ny][nx] += delta) > 1)
+      ok = false;
+  }
+  return ok;
+}
+
+int cover(vector<vector<int>> &board){
+  int y = -1;
+  int x = -1;
+
+  for (int i = 0; i < board.size(); i++){
+    for (int j = 0; j < board[i].size(); j++){
+      if (board[i][j] == 0){
+        y = i;
+        x = j;
+        break;
+      }
+    }
+    if (y != -1)
+      break;
+  }
+
+  //기저사례 : 모든 타일이 검은색이면 1 return.
+  if (y == -1)
+    return 1;
+  
+  int result = 0;
+  for (int type = 0; type < 4; type++){
+    if (set(board, y, x, type, 1))
+      result += cover(board);
+    set(board, y, x, type, -1);
+  }
+  return result;
+}
+
+int main(){
+  int testCase = 0;
+  cin >> testCase;
+
+  int y, x;
+  for (int t = 0; t < testCase; t++){
+    cin >> y >> x;
+    vector<vector<int>> board(y, vector<int>(x, 0));
+    string tmp;
+
+    for (int i = 0; i < y; i++){
+      cin >> tmp;
+      for (int j = 0; j < tmp.size(); j++){
+        if (tmp[j] == '#')
+          board[i][j] = 1;
+      }
+    }
+    cout << cover(board) << endl;
+  }
+  return 0;
+}
