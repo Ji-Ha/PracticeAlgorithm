8383a01895328674132903e97d5e204765b86a8c
Ji-Ha <xodhkd36@gmail.com>
2021-01-18 21:08:36 +0900

    FENCE : 분할정복 정복함

--- 알고리즘_문제해결전략/분할정복/FENCE/FENCE.cpp
+++ 알고리즘_문제해결전략/분할정복/FENCE/FENCE.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
+int fence[20001];
+
 int solution(int left, int right){
+  if(left == right) return fence[left];
+
+  int mid = (left + right) / 2;
+  int result = max(solution(left, mid), solution(mid + 1, right));
   
+  int low = mid;
+  int high = mid + 1;
+  int height = min(fence[low], fence[high]);
+
+  result = max(result, height * 2);
+
+  while(left < low || high < right){
+    if(high < right && (low == left || fence[low - 1] < fence[high + 1])){
+      ++high;
+      height = min(height, fence[high]);
+    }
+    else {
+      --low;
+      height = min(height, fence[low]);
+    }
+
+    result = max(result, height * (high - low + 1));
+  }
+  return result;
 }
 
 int main(){
@@ -13,7 +38,6 @@ int main(){
   for(int t = 0; t < testCase; t++){
     int fenceNumber;
     cin >> fenceNumber;
-    vector<int> fence(fenceNumber);
 
     for(int i = 0; i < fenceNumber; i++){
       int height;
@@ -21,6 +45,7 @@ int main(){
       fence[i] = height;
     }
 
+    cout << solution(0, fenceNumber-1) << '\n';
     
   }
 
