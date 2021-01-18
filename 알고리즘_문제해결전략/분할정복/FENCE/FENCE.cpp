60d1b31cbe615866e279f7d30c2331150e308083
Ji-Ha <xodhkd36@gmail.com>
2021-01-18 20:26:22 +0900

    FENCE(분할정복): 시작

---
+++ 알고리즘_문제해결전략/분할정복/FENCE/FENCE.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int left, int right){
+  
+}
+
+int main(){
+  int testCase;
+  cin >> testCase;
+
+  for(int t = 0; t < testCase; t++){
+    int fenceNumber;
+    cin >> fenceNumber;
+    vector<int> fence(fenceNumber);
+
+    for(int i = 0; i < fenceNumber; i++){
+      int height;
+      cin >> height;
+      fence[i] = height;
+    }
+
+    
+  }
+
+  return 0;
+}
