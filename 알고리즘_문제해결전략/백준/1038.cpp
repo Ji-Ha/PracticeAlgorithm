31d42d62ff418d661c6012ced479e1a6c69af33f
Ji-Ha <xodhkd36@gmail.com>
2021-01-25 21:15:25 +0900

    1038 : dfs

---
+++ 알고리즘_문제해결전략/백준/1038.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+long long n, cnt, dec[1024];
+
+void dfs(long long num){
+    long long val = num % 10, cur = num * 10;
+    for(int i = 0; i < val; i++){
+        dec[cnt++] = cur + i;
+        dfs(cur + i);
+    }
+}
+int main(){
+    scanf("%lld", &n);
+    for(int i = 0; i < 10; i++){
+        dec[cnt++] = i;
+        dfs(i);
+    }
+    sort(dec, dec+cnt);
+    if(n < cnt)printf("%lld", dec[n]);
+    else printf("-1");
+}
