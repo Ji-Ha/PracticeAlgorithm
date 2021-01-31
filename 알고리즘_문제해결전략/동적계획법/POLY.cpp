bc22d0f0056cef4305d065fc9ca487f85b7df878
Ji-Ha <xodhkd36@gmail.com>
2021-01-31 17:02:31 +0900

    느낀점 추가

--- 알고리즘_문제해결전략/동적계획법/POLY.cpp
+++ 알고리즘_문제해결전략/동적계획법/POLY.cpp
@@ -4,6 +4,8 @@ using namespace std;
 const int MOD = 10 * 1000 * 1000;
 int cache[101][101];
 
+// n과 0을 넣었을 때, 값이 모두 구해져 나와서, main문이 좀 더 깔끔해지지 않을까 생각해봤는데 그럴 수 없다.
+// for문을 돌면서 cal이 0이 되는 지점이 있어서 값이 제대로 들어가지 않는다.
 int solution(int n, int first){
   if(n == first) return 1;
   
