a749b7667b179f766cd8edfe9ac0c3b5f07217ec
Ji-Ha <xodhkd36@gmail.com>
2021-02-03 18:05:21 +0900

    MORSE : 동적 계획법

--- 알고리즘_문제해결전략/동적 계획법 테크닉/MORSE.cpp
+++ 알고리즘_문제해결전략/동적 계획법 테크닉/MORSE.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 #include <string>
+#include <memory.h>
+#include <algorithm>
 using namespace std;
-int k;
-int skip;
 
-void generate(int n, int m, string s){
-  if(n == 0 && m == 0){
-    cout << s << '\n';
-    return;
+int bino[201][201];
+const int MAX_VALUE = 1000000000 + 100;
+
+void calcBino(){
+  memset(bino, 0, sizeof(bino));
+
+  for(int i = 0; i <= 200; i++){
+    for(int j = 0; j <= i; j++){
+      if(j == 0 || j == i){
+        bino[i][j] = 1;
+        continue;
+      }
+      bino[i][j] = min(MAX_VALUE, bino[i - 1][j - 1] + bino[i - 1][j]);
+    }
   }
+}
 
-  if(n > 0) generate(n - 1, m, s + "-");
-  if(m > 0) generate(n, m - 1, s + "o");
+string kth(int n, int m, int skip){
+  if(n == 0) return string(m, 'o');
+  if(skip < bino[n + m - 1][n - 1]) return "-" + kth(n - 1, m, skip);
+
+  skip -= bino[n + m - 1][n - 1];
+  return "o" + kth(n, m - 1, skip);
 }
 
 int main(){
+  int skip;
   int c;
   cin >> c;
 
   while(c--){
-    int n, m;
+    int n, m, k;
     cin >> n >> m >> k;
     
-    generate(n, m, "");
+    calcBino();
+    skip = k - 1;
+    cout << kth(n, m, skip) << '\n';
   }
   return 0;
 }
