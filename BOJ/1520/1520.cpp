7a19bb9c2adc03edc1ac8a7a261a642b1d0ab77c
Ji-Ha <xodhkd36@naver.com>
2021-03-20 00:43:18 +0900

    DP : 내리막길

--- BOJ/1520/1520.cpp
+++ BOJ/1520/1520.cpp
@@ -1,38 +1,42 @@
 #include <iostream>
+#include <memory.h>
 using namespace std;
 int m, n;
 int value[500][500];
-bool checker[500][500];
+int dp[500][500];
 
 int solution(int y, int x){
     // 기저사례 1.
     if(y >= m || x >= n || y < 0 || x < 0) return 0;
-    // 기저사례 2.
-    //if(checker[y][x]) return 0;
 
+    int& num = dp[y][x];
+    if(num != -1) return num;
+
+    // 기저사례 2. 마지막에 도착했을 때 1을 돌려준다.
     if(y == m - 1 && x == n - 1) return 1;
 
-    //checker[y][x] = true;
+    num = 0;
 
-    if(value[y + 1][x] < value[y][x]) return solution(y + 1, x);
-    if(value[y - 1][x] < value[y][x]) return solution(y - 1, x);
-    if(value[y][x + 1] < value[y][x]) return solution(y, x + 1);
-    if(value[y][x - 1] < value[y][x]) return solution(y, x - 1);
-
-    return 0;
+    if(value[y + 1][x] < value[y][x]) num = num + solution(y + 1, x);
+    if(value[y - 1][x] < value[y][x]) num = num + solution(y - 1, x);
+    if(value[y][x - 1] < value[y][x]) num = num + solution(y, x - 1);
+    if(value[y][x + 1] < value[y][x]) num = num + solution(y, x + 1);
+    
+    return num;
 }
 
 int main(){
+    memset(dp, -1, sizeof(dp));
     cin >> m >> n;
     int answer = 0;
 
     for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++) cin >> value[m][n];
+        for(int j = 0; j < n; j++){
+            cin >> value[i][j];
+        } 
     }
 
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++) answer += solution(i, j);
-    }
+    answer = solution(0, 0);
 
     cout << answer << '\n';
     return 0;
