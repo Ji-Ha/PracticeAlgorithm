d8077bba8fac2b4dd22cb4e35371865c30637382
Ji-Ha <xodhkd36@naver.com>
2021-03-21 23:42:56 +0900

    DP : 내려가기(완)

--- BOJ/2096/2096.cpp
+++ BOJ/2096/2096.cpp
@@ -3,59 +3,35 @@
 #include <algorithm>
 using namespace std;
 int n;
-int value[100000][3];
-int memoMax[100000][3];
-int memoMin[100000][3];
-
-int solutionMax(int y, int x){
-    //끝에 도착하면 돌아가야지.
-    if(y == n - 1) return value[y][x];
-
-    int& ans = memoMax[y][x];
-
-    if(ans != -1) return ans;
-
-    ans = value[y][x];
-
-    if(x == 0) ans += max(solutionMax(y + 1, x), solutionMax(y + 1, x + 1));
-    if(x == 1) ans += max(solutionMax(y + 1, x - 1), max(solutionMax(y + 1, x), solutionMax(y + 1, x + 1)));
-    if(x == 2) ans += max(solutionMax(y + 1, x - 1), solutionMax(y + 1, x));
-
-    return ans;
-}
-
-int solutionMin(int y, int x){
-    if(y == n - 1) return value[y][x];
-
-    int& ans = memoMin[y][x];
-
-    if(ans != -1) return ans;
-
-    ans = value[y][x];
-
-    if(x == 0) ans += min(solutionMin(y + 1, x), solutionMin(y + 1, x + 1));
-    if(x == 1) ans += min(solutionMin(y + 1, x - 1), min(solutionMin(y + 1, x), solutionMin(y + 1, x + 1)));
-    if(x == 2) ans += min(solutionMin(y + 1, x - 1), solutionMin(y + 1, x));
-
-    return ans;
-}
+int value[3];
+int maxValue[2][3], minValue[2][3];
 
 int main(){
-    memset(memoMax, -1, sizeof(memoMax));
-    memset(memoMin, -1, sizeof(memoMin));
     cin >> n;
-    int maxValue = 0;
-    int minValue = 1000000;
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < 3; j++) cin >> value[i][j];
-    }
-    
-    for(int i = 0; i < 3; i++){
-        maxValue = max(maxValue, solutionMax(0, i));
-        minValue = min(minValue, solutionMin(0, i));
-    }
+        for(int j = 0; j < 3; j++) cin >> value[j];
+        if(i == 0){
+            for(int t = 0; t < 3; t++) maxValue[1][t] = minValue[1][t] = value[t];
+        }
+        else{
+            maxValue[1][0] = max(maxValue[0][0], maxValue[0][1]) + value[0];
+            maxValue[1][1] = max(max(maxValue[0][0], maxValue[0][1]), maxValue[0][2]) + value[1];
+            maxValue[1][2] = max(maxValue[0][1], maxValue[0][2]) + value[2];
 
-    cout << maxValue << ' ' << minValue << '\n';
+            minValue[1][0] = min(minValue[0][0], minValue[0][1]) + value[0];
+            minValue[1][1] = min(min(minValue[0][0], minValue[0][1]), minValue[0][2]) + value[1];
+            minValue[1][2] = min(minValue[0][1], minValue[0][2]) + value[2];
+        }
+        for(int t = 0; t < 3; t++){
+            maxValue[0][t] = maxValue[1][t];
+            minValue[0][t] = minValue[1][t];
+        }
+    }
+    int ansMax = max(max(maxValue[0][0], maxValue[0][1]), maxValue[0][2]);
+    int ansMin = min(min(minValue[0][0], minValue[0][1]), minValue[0][2]);
+
+
+    cout << ansMax << ' ' << ansMin << '\n';
     return 0;
 }
