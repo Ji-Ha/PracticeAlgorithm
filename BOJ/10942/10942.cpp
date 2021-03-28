95022c8b63a4de389155f94b5f90f16b95b55fc3
Ji-Ha <xodhkd36@naver.com>
2021-03-28 19:42:42 +0900

    펠린드롬 ? : dp

--- BOJ/10942/10942.cpp
+++ BOJ/10942/10942.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
-#include <memory.h>
 using namespace std;
+int n;
 int value[2001];
 int memo[2001][2001];
 
-int solution(int start, int end){
-    int& res = memo[start][end];
-    if(start >= end) return 1;
-
-    if(res != -1) return res;
-
-    res = 0;
-
-    if(value[start] != value[end]) return res;
-    else{
-        return res = solution(start + 1, end - 1);
+void init(){
+    for(int i = 1; i <= n; i++) memo[i][i] = 1;
+    for(int i = 1; i < n ; i++) if(value[i] == value[i + 1]) memo[i][i + 1] = 1;
+    for(int i = 3; i <= n; i++){
+        for(int j = 1; j <= n - j + 1; j++){
+            int k = i + j - 1;
+            if(value[k] == value[j] && memo[j + 1][k - 1]) memo[j][k] = 1;
+        }
     }
 }
 
 int main(){
-    int n;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n;
-    memset(memo, -1, sizeof(memo));
-
     for(int i = 1; i <= n; i++) cin >> value[i];
 
     int m;
     cin >> m;
 
+    for(int i = 1; i <= n; i++) memo[i][i] = 1;
+    for(int i = 1; i < n ; i++){
+        if(value[i] == value[i + 1]) memo[i][i + 1] = 1;
+    } 
+    for(int i = 3; i <= n; i++){
+        for(int j = 1; j <= n - i + 1; j++){
+            int k = i + j - 1;
+            if(value[j] == value[k] && memo[j + 1][k - 1]) memo[j][k] = 1;
+        }
+    }
+
     while(m--){
         int s, e;
         cin >> s >> e;
-        cout << solution(s, e) << "\n";
+        cout << memo[s][e] << "\n";
     }
     return 0;
 }
