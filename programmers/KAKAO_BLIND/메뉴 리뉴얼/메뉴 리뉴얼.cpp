6de157310670abf81c86076e3501b72a872e3d1b
Ji-Ha <xodhkd36@naver.com>
2021-04-05 18:29:59 +0900

    백트래킹, 조합

--- programmers/KAKAO_BLIND/메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ programmers/KAKAO_BLIND/메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -1,18 +1,25 @@
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <unordered_map>
 #include <algorithm>
+
 using namespace std;
 
 int cnt[27];
-unordered_map<string, int> check;
+unordered_map<string, int> um;
 vector<string> menu[27][21];
 
 void comb(string s, int idx, string made){
     if(made.size() > 1){
-        check[made]++;
-        cnt[made.size()]=max(cnt[made.size()], check[made]);
+        um[made]++;
+        cnt[made.size()] = max(cnt[made.size()], um[made]);
+        menu[made.size()][um[made]].push_back(made);
+    }
+    
+    for(int i = idx + 1; i < s.size(); i++){
+        made.push_back(s[i]);
+        comb(s, i, made);
+        made.pop_back();
     }
 }
 
@@ -20,26 +27,16 @@ vector<string> solution(vector<string> orders, vector<int> course) {
     vector<string> answer;
     for(string& s : orders){
         sort(s.begin(), s.end());
+        comb(s, -1, "");
     }
+    
+    for(int i : course){
+        if(cnt[i] > 1){
+            for(string s : menu[i][cnt[i]])
+                answer.push_back(s);
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
     return answer;
-}
-
-int main(){
-    vector<string> s;
-    vector<int> c;
-
-    for(int i = 0; i < 6; i++){
-        string temp;
-        cin >> temp;
-        s.push_back(temp);
-    }
-
-    for(int i = 0; i < 3; i++){
-        int temp;
-        cin >> temp;
-        c.push_back(temp);
-    }
-
-    solution(s, c);
-    return 0;
 }
