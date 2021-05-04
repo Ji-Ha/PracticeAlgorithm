88f04d4f95cfb5b0a3dc7069e236aee8abfb4aa4
Ji-Ha <xodhkd36@naver.com>
2021-05-04 23:48:51 +0900

    quad

--- programmers/code challenge/quad.cpp
+++ programmers/code challenge/quad.cpp
@@ -1,7 +1,30 @@
- #include <iostream>
- using namespace std;
+#include <string>
+#include <vector>
 
- int main(){
+using namespace std;
+vector<vector<int>> a;
+vector<int> ans;
 
-     return 0;
- }
+int check(int y, int x, int length){
+    int stat = a[y][x];
+    for(int i = y; i < y + length; i++){
+        for(int j = x; j < x + length; j++){
+            if(stat != a[y][x]) return -1;
+        }
+    }
+    return stat;
+}
+
+void block(int y, int x, int length){
+    int checker = check(y, x, length);
+    if(checker == -1){
+        block()
+    }
+    ans.push_back(checker);
+}
+
+vector<int> solution(vector<vector<int>> arr) {
+    vector<int> answer;
+    a = arr;
+    return answer;
+}
