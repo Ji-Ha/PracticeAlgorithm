68dab8fd57c78ce1e0e0a44353880c7655d39e2b
Ji-Ha <xodhkd36@naver.com>
2021-06-21 13:31:53 +0900

    11066(파일합치기) : dp

--- BOJ/11066/11066.cpp
+++ BOJ/11066/11066.cpp
@@ -13,17 +13,18 @@ int main(){
     while(t--){
         int k;
         cin >> k;
+        
         for(int i = 1; i <= k; i++){
             cin >> value[i];
             summary[i] = summary[i - 1] + value[i];
         }
 
-        for(int i = 1; i < k; i++){
+        for(int i = 1; i <= k; i++){
             for(int tx = 1; tx + i <= k; tx++){
                 int ty = tx + i;
                 memo[tx][ty] = INT_MAX;
 
-                for(int mid = tx; mid < ty; tx++)
+                for(int mid = tx; mid < ty; mid++)
                     memo[tx][ty] = min(memo[tx][ty], memo[tx][mid] + memo[mid + 1][ty] + summary[ty] - summary[tx - 1]);
             }
         }
